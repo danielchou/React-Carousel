13234f065394522dab4de690168343eab1f0ccef
daniel.chou <daniel0422@gmail.com>
2015-11-03 10:56:12 +0800

    init.

---
+++ main.js
@@ -0,0 +1,11 @@
+
+var $ = require("jquery/src/core");
+var jQuery = $;
+var carousel=require("bootstrap/js/carousel");
+
+document.write('<h1>Hello World</h1>');
+
+$(function(){
+	alert("asdfasdf");
+	$('.carousel').carousel();	
+});
